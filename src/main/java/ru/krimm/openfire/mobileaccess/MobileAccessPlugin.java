f1e529550980d660596cceb5dcb0fde062eec7ce
s-kholman <102976069+s-kholman@users.noreply.github.com>
2026-07-18 13:36:24 +0500

    feat: expose audited administration service to console

--- src/main/java/ru/krimm/openfire/mobileaccess/MobileAccessPlugin.java
+++ src/main/java/ru/krimm/openfire/mobileaccess/MobileAccessPlugin.java
@@ -3,13 +3,13 @@ package ru.krimm.openfire.mobileaccess;
 import java.io.File;
 import java.time.Clock;
 import java.util.Objects;
-
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
 import org.jivesoftware.util.JiveGlobals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import ru.krimm.openfire.mobileaccess.admin.MobileAccessAdministrationService;
+import ru.krimm.openfire.mobileaccess.audit.JdbcAuditRepository;
 import ru.krimm.openfire.mobileaccess.credential.JdbcMobileCredentialRepository;
 import ru.krimm.openfire.mobileaccess.credential.MobileAccessSchemaManager;
 import ru.krimm.openfire.mobileaccess.credential.MobileCredentialService;
@@ -24,10 +24,12 @@ public final class MobileAccessPlugin implements Plugin {
     static final String DEFAULT_ALLOWED_GROUP = "Openfire-Users";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MobileAccessPlugin.class);
+    private static volatile MobileAccessPlugin instance;
 
     private File pluginDirectory;
     private DirectoryEligibilityService directoryEligibilityService;
     private MobileCredentialService mobileCredentialService;
+    private MobileAccessAdministrationService administrationService;
 
     @Override
     public void initializePlugin(final PluginManager pluginManager, final File pluginDirectory) {
@@ -36,39 +38,45 @@ public final class MobileAccessPlugin implements Plugin {
 
         new MobileAccessSchemaManager().ensureSchema();
 
+        final Clock clock = Clock.systemUTC();
         final String allowedGroup = JiveGlobals.getProperty(ALLOWED_GROUP_PROPERTY, DEFAULT_ALLOWED_GROUP);
         directoryEligibilityService = new DirectoryEligibilityService(new OpenfireDirectoryGateway(), allowedGroup);
         mobileCredentialService = new MobileCredentialService(
             directoryEligibilityService,
             new JdbcMobileCredentialRepository(),
             new Pbkdf2PasswordHasher(),
-            Clock.systemUTC()
+            clock
         );
+        administrationService = new MobileAccessAdministrationService(
+            mobileCredentialService,
+            new JdbcAuditRepository(),
+            clock
+        );
+        instance = this;
 
-        LOGGER.info(
-            "Mobile Access plugin initialized from {} with allowed directory group '{}'",
-            pluginDirectory.getAbsolutePath(),
-            allowedGroup
-        );
+        LOGGER.info("Mobile Access plugin initialized from {} with allowed directory group '{}'",
+            pluginDirectory.getAbsolutePath(), allowedGroup);
     }
 
     @Override
     public void destroyPlugin() {
         LOGGER.info("Mobile Access plugin destroyed");
+        instance = null;
+        administrationService = null;
         mobileCredentialService = null;
         directoryEligibilityService = null;
         pluginDirectory = null;
     }
 
-    File getPluginDirectory() {
-        return pluginDirectory;
+    public static MobileAccessAdministrationService administrationService() {
+        final MobileAccessPlugin current = instance;
+        if (current == null || current.administrationService == null) {
+            throw new IllegalStateException("Mobile Access plugin is not initialized");
+        }
+        return current.administrationService;
     }
 
-    DirectoryEligibilityService getDirectoryEligibilityService() {
-        return directoryEligibilityService;
-    }
-
-    MobileCredentialService getMobileCredentialService() {
-        return mobileCredentialService;
-    }
+    File getPluginDirectory() { return pluginDirectory; }
+    DirectoryEligibilityService getDirectoryEligibilityService() { return directoryEligibilityService; }
+    MobileCredentialService getMobileCredentialService() { return mobileCredentialService; }
 }
