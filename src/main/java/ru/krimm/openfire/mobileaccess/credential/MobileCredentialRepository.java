cb15fd7bf6093045e44ae51cbe7aa2ea33fde94f
s-kholman <102976069+s-kholman@users.noreply.github.com>
2026-07-19 20:16:50 +0500

    feat: extend credential repository lifecycle

--- src/main/java/ru/krimm/openfire/mobileaccess/credential/MobileCredentialRepository.java
+++ src/main/java/ru/krimm/openfire/mobileaccess/credential/MobileCredentialRepository.java
@@ -1,6 +1,7 @@
 package ru.krimm.openfire.mobileaccess.credential;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface MobileCredentialRepository {
@@ -9,5 +10,11 @@ public interface MobileCredentialRepository {
 
     void revoke(String username, Instant revokedAt);
 
+    void enable(String username, Instant changedAt);
+
+    void delete(String username);
+
     Optional<MobileCredentialRecord> find(String username);
+
+    List<MobileCredentialRecord> findAll();
 }
