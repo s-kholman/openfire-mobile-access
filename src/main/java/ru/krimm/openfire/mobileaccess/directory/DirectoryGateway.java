d20c645f135e31dafc6a3295e96fffebf9a34e61
s-kholman <102976069+s-kholman@users.noreply.github.com>
2026-07-19 22:27:26 +0500

    refactor: remove redundant group checks

--- src/main/java/ru/krimm/openfire/mobileaccess/directory/DirectoryGateway.java
+++ src/main/java/ru/krimm/openfire/mobileaccess/directory/DirectoryGateway.java
@@ -2,14 +2,8 @@ package ru.krimm.openfire.mobileaccess.directory;
 
 import java.util.Optional;
 
-/**
- * Boundary between plugin business logic and Openfire user/group providers.
- */
+/** Boundary between plugin business logic and the configured Openfire UserProvider. */
 public interface DirectoryGateway {
 
     Optional<DirectoryUser> findUser(String username);
-
-    boolean groupExists(String groupName);
-
-    boolean isMemberOf(String username, String groupName);
 }
